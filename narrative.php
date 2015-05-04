dcc2ae17c6bc72389993c69d0db181e72bbdc34a
algoldma <aidan.goldman@uvm.edu>
2015-05-04 17:17:19 -0400

    print style FINAL

--- narrative.php
+++ narrative.php
@@ -14,7 +14,7 @@
     </head>
     <body id="home">
         <article id="main">
-            <h1>Web Usability Test |  <a href="https://lbrown20.w3.uvm.edu/cs142/assignment7/index.html">LoVermont</a></h1>
+            <h1>Web Usability Test |  <a href="https://algoldma.w3.uvm.edu/cs142/assignment7/index.html">LoVermont Hiking Project</a></h1>
 
             <nav>
                 <ul id='shadowmenu'>
@@ -29,8 +29,8 @@
 
             <h2>Before...</h2>
             <div class='container1' > 
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment7/HomeBefore.png" target="_blank"><img src="HomeBefore.png" width="300" height="200"  alt="Main Page" /></a>
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment7/MostPopularBefore.png" target="_blank"><img src="MostPopularBefore.png" width="300" height="200"  alt="Most Popular" /></a>
+                <img src="https://cbologni.w3.uvm.edu/cs142/assignment7/HomeBefore.png" width="300" height="200"  alt="Main Page" />
+                <img src="https://cbologni.w3.uvm.edu/cs142/assignment7/MostPopularBefore.png" width="300" height="200"  alt="Most Popular" />
 
             </div>
 
@@ -39,11 +39,16 @@
                 <div class="container">
                     <h2>5 Changes</h2>
                     <ol class="ordered-list">
-                        <li>Change 1: Added a drop down menu to show which hikes would appear under each category on the main navigation</li>
-                        <li>Change 2: Added trail maps to each of the hikes </li>
-                        <li>Change 3: Changed the link to the UVM Outing Club so it would open a new window</li>
-
-
+                        <li>Change 1: Added a drop down menu to show which hikes would appear under each category on the main navigation. This contributed to making the navigation more overall consistent and the site cleaner, which the users made note of.</li>
+                        <li>Change 2: Added photos and trail maps to each of the hikes, to distinguish between them and make each hike's page more specific. </li>
+                        <li>Change 3: Changed the link to the UVM Outing Club so it would open a new window. Users mentioned that they wished they could have stayed on our site and just had this outside link appear in a new window, so that they could continue to browse our site. We followed suit on this for all outside links on the site after this usability test.</li>
+                        <li>Change 4: Changed the Resources Page to feature links of outside resources rather than just general hiking tips. Users were confused since most Resources pages are conventionally used for this purpose and not for tips.</li>
+                        <li>Change 5: We fixed around the Home Page with a new description block, put the tips for hiking under that description, changed the name of the site to something more specific "LoVermont" to "LoVermont Hiking Project"</li>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <li>Change 6(After Second Test): Added thumbnails of photos to include in the buttons to get to each hike page. This indicates clearly where the button goes and gives the user a glimpse of the mountain so they can chose one that looks appealing to them. </li>
+                        <li>Change 7(After Second Test): Created hover effects for the drop down nav menu, including rounded edges and lighter highlighting on the menu options. </li>
 
                     </ol>
                 </div>
@@ -52,11 +57,28 @@
 
             <h2>After...</h2>
             <div class='container2'>
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment6/finalch1.gif" target="_blank"><img src="finalch1.gif" width="300" height="200"  alt="Home Page" /></a>
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment6/finalch2.gif" target="_blank"><img src="finalch2.gif" width="300" height="200"  alt="Home Page" /></a>
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment6/finalch3.gif" target="_blank"><img src="finalch3.gif" width="300" height="200"  alt="New Nav" /></a>
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment6/finalch4.gif" target="_blank"><img src="finalch4.gif" width="300" height="200"  alt="Gallery Page" /></a>
-                <a href="https://lbrown20.w3.uvm.edu/cs142/assignment6/final.gif" target="_blank"><img src="final.gif" width="600" height="400"  alt="Products Page" /></a>
+                
+            <div id="screenshot1"></div>
+<p><center>(Rollover the image to see first alteration)</center></p>
+        <p><center>Change 1</center></p>
+<div id="screenshot2"></div>
+       <p><center>(Rollover the image to see second alteration)</center></p>
+        <p><center>Change 2</center></p>
+<div id="screenshot3"></div>
+       <p><center>(Rollover the image to see third alteration)</center></p>
+       <p><center>Change 3</center> </p>
+       <div id="screenshot4"></div>
+       <p><center>(Rollover the image to see fourth alteration)</center></p>
+       <p><center>Change 4</center></p>
+<div id="screenshot5"></div>
+       <p><center>(Rollover the image to see fifth alteration)</center></p>
+       <p><center>Change 5</center></p>         
+
+                <img src="https://cbologni.w3.uvm.edu/cs142/assignment7/NavAfter.png" width="300" height="200"  alt="Home Page" />
+                <img src="https://cbologni.w3.uvm.edu/cs142/assignment7/PhotosAfter.png" width="300" height="200"  alt="Home Page" />
+                <img src="https://cbologni.w3.uvm.edu/cs142/assignment7/OutingClubAfter.png" width="300" height="200"  alt="New Nav" />
+                <img src="https://cbologni.w3.uvm.edu/cs142/assignment7/siteshot5r.png" width="300" height="200"  alt="Gallery Page" />
+       
 
             </div>
 
